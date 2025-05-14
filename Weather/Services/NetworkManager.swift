62c5767109aee5a55fad2eb46dbbdf10c0b7d3b6
Ilias Almakaev <i.almakaev@gmail.com>
2025-05-14 19:12:11 +0700

    Prepare request

--- Weather/Services/NetworkManager.swift
+++ Weather/Services/NetworkManager.swift
@@ -6,10 +6,25 @@
 //
 
 import Foundation
+import Alamofire
 
 final class NetworkManager {
   
   static let shared = NetworkManager()
   
   private init() {}
+  
+  func getWeatherInfo(withCity city: String, completion: @escaping(Result<WeatherInfo, AFError>) -> Void) {
+    guard let url = URL(string: "http://api.weatherapi.com/v1/forecast.json?key=fa8b3df74d4042b9aa7135114252304&q=\(city)&days=7")
+    else {
+      completion(.failure(AFError.invalidURL(url: URL(string: "")!)))
+      return
+    }
+    
+    AF.request(url)
+      .validate()
+      .responseDecodable(of: WeatherInfo.self) { response in
+        // TODO: - Обработать результат
+      }
+  }
 }
